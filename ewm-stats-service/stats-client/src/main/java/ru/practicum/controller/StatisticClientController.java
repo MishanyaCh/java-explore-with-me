45c1bd27165a7e568cce89249ec14e69e39d038d
MishanyaCh <128893565+MishanyaCh@users.noreply.github.com>
2024-08-04 17:56:44 +0300

    refact: откорректировал клиента для работы с основным сервисом

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/controller/StatisticClientController.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/controller/StatisticClientController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.StatDto;
+import ru.practicum.ViewStatDto;
 import ru.practicum.client.StatisticClient;
 
 import javax.validation.Valid;
@@ -30,21 +30,21 @@ public class StatisticClientController {
 
     @PostMapping(path = "/hit")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public ResponseEntity<Object> saveInformation(@RequestBody @Valid StatDto statDto) {
+    public StatDto saveInformation(@RequestBody @Valid StatDto statDto) {
         log.info("Пришел POST /hit запрос с телом: {}", statDto);
-        final ResponseEntity<Object> savedInfo = statisticClient.saveInformation(statDto);
-        log.info("На POST /hit запрос отправлен ответ с телом: {}", savedInfo.getBody());
+        final StatDto savedInfo = statisticClient.saveInformation(statDto);
+        log.info("На POST /hit запрос отправлен ответ с телом: {}", savedInfo);
         return savedInfo;
     }
 
     @GetMapping(path = "/stats")
-    public ResponseEntity<Object> getStatistic(@RequestParam String start, @RequestParam String end,
-                                               @RequestParam(required = false) List<String> uris,
-                                               @RequestParam Boolean unique) {
+    public List<ViewStatDto> getStatistic(@RequestParam String start, @RequestParam String end,
+                                          @RequestParam(required = false) List<String> uris,
+                                          @RequestParam Boolean unique) {
         log.info("Пришел GET /stats?start={}&end={}&uris={}&unique={} запрос", start, end, uris, unique);
-        final ResponseEntity<Object> result = statisticClient.getStatistic(start, end, uris, unique);
+        final List<ViewStatDto> result = statisticClient.getStatistic(start, end, uris, unique);
         log.info("На GET /stats?start={}&end={}&uris={}&unique={} запрос отправлен ответ: {}",
-                start, end, uris, unique, result.getBody());
+                start, end, uris, unique, result);
         return result;
     }
 }
